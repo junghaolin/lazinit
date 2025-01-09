e276c8fa8dcf125e207b689816822dfda3fd2410
junghaolin <junghaolin@gmail.com>
2025-01-09 14:00:39 +0800

    download JetBrainsMono when install it; ghostty; kitty in neovim

--- zsh/fonts/install.sh
+++ zsh/fonts/install.sh
@@ -2,4 +2,9 @@
 sudo apt install unzip fontconfig -y
 mkdir -p ~/.local/share/fonts
 unzip SourceCodePro.zip -d ~/.local/share/fonts/
+unzip Hack.zip -d ~/.local/share/fonts/
+unzip FiraMono.zip -d ~/.local/share/fonts/
+wget https://github.com/JetBrains/JetBrainsMono/releases/download/v2.304/JetBrainsMono-2.304.zip
+unzip JetBrainsMono-2.304.zip -d ~/.local/share/fonts/
+
 fc-cache ~/.local/share/fonts
